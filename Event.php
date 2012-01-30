170e68d39f03476d230e9e673a4a672b86eb8717
Dan Gilbert <ddg@daentech.co.uk>
2012-01-30 06:30:57 +0000

    Added location description so we have 2 ways of referring to a location now

--- Event.php
+++ Event.php
@@ -1,27 +1,29 @@
 <?php
+
 /*
  * Created on 30 Jan 2012
  *
  * To change the template for this generated file go to
  * Window - Preferences - PHPeclipse - PHP - Code Templates
  */
- 
- // Event class. This stores all the required details for each event provided
- 
- class Event{
- 	
- 	var $mStart, $mEnd, $mTitle, $mDesc, $mLocation, $mLecturer, $mLon, $mLat, $mType;
- 	
- 	public function __construct($start, $end, $title, $desc, $location, $lecturer, $lon, $lat, $type){
- 		$this->mStart = $start;
- 		$this->mEnd = $end;
- 		$this->mTitle = $title;
- 		$this->mDesc = $desc;
- 		$this->mLocation = $location;
- 		$this->mLecturer = $lecturer;
- 		$this->mLon = $lon;
- 		$this->mLat = $lat;
- 		$this->mType = $type;
- 	}
- }
+
+// Event class. This stores all the required details for each event provided
+
+class Event {
+
+	var $mStart, $mEnd, $mTitle, $mDesc, $mLocation, $mLocationDesc, $mLecturer, $mLon, $mLat, $mType;
+
+	public function __construct($start, $end, $title, $desc, $location, $locationDesc, $lecturer, $lon, $lat, $type) {
+		$this->mStart = $start;
+		$this->mEnd = $end;
+		$this->mTitle = $title;
+		$this->mDesc = $desc;
+		$this->mLocation = $location;
+		$this->mLocationdesc = $locationDesc;
+		$this->mLecturer = $lecturer;
+		$this->mLon = $lon;
+		$this->mLat = $lat;
+		$this->mType = $type;
+	}
+}
 ?>
