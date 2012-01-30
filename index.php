ddcb8edd54bdf66e4583ac7374318155c84d00fa
Dan Gilbert <ddg@daentech.co.uk>
2012-01-30 06:31:57 +0000

    Fixed the form to allow the sending of the JSON now

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+
 /*
  * Created on 30 Jan 2012
  *
@@ -10,9 +11,16 @@
 // return the calendar upon submission (using AJAX?)
 
 // URL: https://portal.bris.ac.uk/studenttimetabling/timetable.json?start=1327881600&end=1337558400
+ini_set('display_errors', 1);
+ini_set('log_errors', 1);
+ini_set('error_log', dirname(__FILE__) . '/error_log.txt');
+error_reporting(E_ALL);
 ?>
-
-<form action="createCal.php" method="post"> 
-<textarea id="calData" ></textarea>
-<input type="submit" value="Get my Calendar!"
+<html>
+<body>
+<form name="form" action="createCal.php" method="post"> 
+<textarea name="calData" ></textarea>
+<input type="submit" value="Get my Calendar!" />
 </form>
+</body>
+</html>
