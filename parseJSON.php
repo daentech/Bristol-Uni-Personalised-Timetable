8154aa15acac949d4a368b8662d274ea4a859b12
Dan Gilbert <ddg@daentech.co.uk>
2012-01-30 06:49:56 +0000

    Now reading all values from the JSON into an Event array

--- parseJSON.php
+++ parseJSON.php
@@ -1,5 +1,6 @@
 <?php
 
+
 /*
  * Created on 30 Jan 2012
  *
@@ -9,33 +10,59 @@
 
 // Parse the provided JSON from the uni calendar into php objects
 
-require_once('pear/JSON.php');
-require_once('Event.php');
+require_once ('pear/JSON.php');
+require_once ('Event.php');
 
 class parseJSON {
 
 	//var $mJSON;
 
 	function parse($json) {
-		
+
 		$badjson = new Services_JSON();
-	$data = $badjson->decode($json); 
-    
-    $loc = $data[0]->{'locs'}[0]->{'loc'}; // Location Short code
-    $locdesc =  $data[0]->{'locs'}[0]->{'locdesc'}; // Location full name
-    $title = $data[0]->{'title'}; // Unit code
-    $name = $data[0]->{'desc'}; // Unit title
-    $staff = $data[0]->{'allStaff'}[0]->{'staffName'}; // Lecturer name
-    $type = $data[0]->{'type'}; // Event type
-    $start = $data[0]->{'start'}; // Start date and time
-    $end = $data[0]->{'end'}; // End date and time
-    $lng = $data[0]->{'sites'}[0]->{'lng'}; // Longitude
-    $lat = $data[0]->{'sites'}[0]->{'lat'}; // Latitude
-    
-    $eventArr[] = new Event($start, $end, $title, $name, $loc, $locdesc, $staff, $lng, $lat, $type);
-	
-	var_dump($eventArr);
-	return $eventArr;	
+		$data = $badjson->decode($json);
+		$size = sizeof($data);
+		echo "Data size: ".$size."<br />";
+		$eventArr = array();
+		for ($i = 0; $i < $size; $i++) {
+
+			$loc = $data[$i]-> {
+				'locs' }
+			[0]-> {
+				'loc' }; // Location Short code
+			$locdesc = $data[$i]-> {
+				'locs' }
+			[0]-> {
+				'locdesc' }; // Location full name
+			$title = $data[$i]-> {
+				'title' }; // Unit code
+			$name = $data[$i]-> {
+				'desc' }; // Unit title
+			$staff = $data[$i]-> {
+				'allStaff' }
+			[0]-> {
+				'staffName' }; // Lecturer name
+			$type = $data[$i]-> {
+				'type' }; // Event type
+			$start = $data[$i]-> {
+				'start' }; // Start date and time
+			$end = $data[$i]-> {
+				'end' }; // End date and time
+			$lng = $data[$i]-> {
+				'sites' }
+			[0]-> {
+				'lng' }; // Longitude
+			$lat = $data[$i]-> {
+				'sites' }
+			[0]-> {
+				'lat' }; // Latitude
+
+			$eventArr[] = new Event($start, $end, $title, $name, $loc, $locdesc, $staff, $lng, $lat, $type);
+
+			var_dump($eventArr);
+		}
+		echo sizeof($eventArr);
+		return $eventArr;
 	}
 
 }
